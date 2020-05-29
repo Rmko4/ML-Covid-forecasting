01d41f6a39396a5380cc798cdc48334ad6bf5b75
Remco Leijenaar <remco-leijenaar@live.com>
2020-05-29 13:41:34 +0200

    Load in data and rough preprocessing'

--- Network/torch_network.py
+++ Network/torch_network.py
@@ -1,23 +1,47 @@
 from pathlib import Path
-import torch
-import torch.nn as nn
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import torch
+#import torch.nn as nn
 
 CSV_PATH = Path("Data/COVID-19.csv")
 
-def getData(path):
+
+def get_data(path):
+    country_data = []
+
     data_path = Path.cwd() / path
-    print(data_path)
-    df = pd.read_csv(data_path, usecols=["cases", "deaths", "countryterritoryCode"])
-    return df
+    data_frame = pd.read_csv(data_path, usecols=[
+        "cases", "deaths", "countriesAndTerritories", "dateRep"])
+    group = data_frame.groupby("countriesAndTerritories")
+
+    for country, df_country in group:
+        cases = df_country["cases"]
+        last_index = cases.to_numpy().nonzero()[0][-1] + 1
+        df_trimmed = df_country.drop(df_country.index[last_index:])
+        df_reversed = df_trimmed.iloc[::-1]
+        country_data.append((country, df_reversed))
+        
+    return country_data
+
+
+def plot_data(country_data):
+    for country, df_country in country_data:
+        if country == "Germany":
+            df_country.plot(x="dateRep")
+            plt.grid()
+            plt.title("Country: " + country)
+            plt.ylabel('Number of people')
+            plt.xlabel('Date')
+            plt.show()
+
 
 def main():
-    data = getData(CSV_PATH)
+    data = get_data(CSV_PATH)
+    #plot_data(data)
+
 
 if __name__ == "__main__":
     main()
-    
-
-    pass
