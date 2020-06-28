0e1ca9deb1d7335bc3d6b63d13fbd5be42d4f7df
Remco Leijenaar <remco-leijenaar@live.com>
2020-06-28 23:15:59 +0200

    Added weight and activation structures

--- Network/mlp.c
+++ Network/mlp.c
@@ -6,11 +6,24 @@
 
 #define STRLEN 30
 #define DATELEN 9
+#define LAYERS 3
 
 typedef struct matrixstruct {
   int rows, columns;
   double **matrix;
 } * Matrix;
+
+typedef struct weightstruct {
+  int layers; // Amount of layers in mlp
+  int *size;  // size per layer
+  Matrix *W;  // Weights matrices
+  double **b; // Bias vectors
+} Weight;
+
+typedef struct mlpstruct {
+  double **x; // activation
+  Weight weight;
+} MLP;
 
 void *safeMalloc(int n) {
   void *ptr = malloc(n);
@@ -33,6 +46,28 @@ Matrix makeMatrix(int r, int c) {
   return A;
 }
 
+MLP makeMLP(int layers, int *size) {
+  MLP model;
+  Weight weight;
+
+  weight = model.weight;
+  weight.layers = layers;
+  weight.size = size;
+
+  weight.W = safeMalloc((layers - 1) * sizeof(Matrix));
+  weight.b = safeMalloc((layers - 1) * sizeof(double));
+  model.x = safeMalloc(layers * sizeof(double));
+
+  // Weight layer k is at k - 1
+  for (int k = 0; k < layers - 1; k++) {
+    weight.W[k] = makeMatrix(size[k + 1], size[k]);
+    weight.b[k] = safeMalloc(size[k + 1]);
+    model.x = safeMalloc(size[k] * sizeof(double));
+  }
+  // Output layer activation units.
+  model.x = safeMalloc(size[layers - 1] * sizeof(double));
+}
+
 void freeMatrix(Matrix A) {
   free(A->matrix[0]);
   free(A->matrix);
@@ -41,8 +76,9 @@ void freeMatrix(Matrix A) {
 
 double sigmoid(double x) { return 1 / (1 + exp(-x)); }
 
-Matrix *readDataFile(char *fileName, int *nSeries, char ***names) {
-  int nVariate, nTimeSteps;
+Matrix *readDataFile(char *fileName, int *nSeries, int *nVariate,
+                     char ***names) {
+  int nTimeSteps;
   Matrix *Sample;
   FILE *fp;
 
@@ -54,7 +90,7 @@ Matrix *readDataFile(char *fileName, int *nSeries, char ***names) {
   }
 
   fscanf(fp, "%d", nSeries);
-  fscanf(fp, "%d", &nVariate);
+  fscanf(fp, "%d", nVariate);
 
   *names = safeMalloc(*nSeries * sizeof(char *));
   Sample = safeMalloc(*nSeries * sizeof(Matrix));
@@ -64,10 +100,10 @@ Matrix *readDataFile(char *fileName, int *nSeries, char ***names) {
     fscanf(fp, "%s", (*names)[i]);
     fscanf(fp, "%d", &nTimeSteps);
 
-    Sample[i] = makeMatrix(nTimeSteps, nVariate);
+    Sample[i] = makeMatrix(nTimeSteps, *nVariate);
 
     for (int r = 0; r < nTimeSteps; r++) {
-      for (int c = 0; c < nVariate; c++) {
+      for (int c = 0; c < *nVariate; c++) {
         fscanf(fp, "%lf", &(Sample[i]->matrix[r][c]));
       }
     }
@@ -78,40 +114,54 @@ Matrix *readDataFile(char *fileName, int *nSeries, char ***names) {
   return Sample;
 }
 
-int main(int argc, char *argv[]) {
-  int windowSize, nUnits, nSeries;
+int intParse(char *arg) {
+  char *end;
   long strParse;
-  char **names;
-  char *end, *fileName;
+
+  strParse = strtol(arg, &end, 10);
+  errno = 0;
+
+  if (errno != 0 || *end != '\0' || strParse > INT_MAX) {
+    perror("Error while converting arg.\n");
+    exit(EXIT_FAILURE);
+  }
+  return (int)strParse;
+}
+
+int main(int argc, char *argv[]) {
+  int windowSize, nSeries, nVariate;
+  int size[LAYERS], nHiddenUnits;
+  char *fileName, **names;
+
   Matrix *sample;
+  MLP model;
 
   if (argc < 3) {
     printf("Provide args: <filename> <window length> <number hidden units>");
     exit(EXIT_FAILURE);
   }
 
-  errno = 0;
-  fileName = argv[1];
-  strParse = strtol(argv[2], &end, 10);
+  windowSize = intParse(argv[2]);
+  nHiddenUnits = intParse(argv[3]);
 
-  if (errno != 0 || *end != '\0' || strParse > INT_MAX) {
-    perror("Error while converting arg.\n");
-    exit(EXIT_FAILURE);
-  }
+  sample = readDataFile(fileName, &nSeries, &nVariate, &names);
 
-  windowSize = strParse;
-  strParse = strtol(argv[3], &end, 10);
+  size[0] = nVariate * windowSize;
+  size[1] = nHiddenUnits;
+  size[2] = nVariate;
 
-  if (errno != 0 || *end != '\0' || strParse > INT_MAX || strParse < 1) {
-    perror("Error while converting arg.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  nUnits = strParse;
-
-  sample = readDataFile(fileName, &nSeries, &names);
+  model = makeMLP(LAYERS, size);
 
   for (int i = 0; i < nSeries; i++) {
+    freeMatrix(sample[i]);
+  }
+  free(sample);
+  // if (windowSize > A->rows - 1) {
+  //  printf("Length of the window is too large.");
+  //  exit(EXIT_FAILURE);
+  //}
+
+  /*for (int i = 0; i < nSeries; i++) {
     printf("%s\n", names[i]);
     for (int r = 0; r < sample[i]->rows; r++) {
       for (int c = 0; c < sample[i]->columns; c++) {
@@ -119,17 +169,6 @@ int main(int argc, char *argv[]) {
       }
       printf("\n");
     }
-  }
-
-  for (int i = 0; i < nSeries; i++) {
-    freeMatrix(sample[i]);
-  }
-  free(sample);
-
-  //if (windowSize > A->rows - 1) {
-  //  printf("Length of the window is too large.");
-  //  exit(EXIT_FAILURE);
-  //}
-
+  }*/
   return 0;
 }
