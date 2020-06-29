c8e5996dc7ce80b116e3c7d69234b6d1e8269618
Remco Leijenaar <remco-leijenaar@live.com>
2020-06-29 16:49:12 +0200

    Back propagation algorithm added

--- Network/mlp.c
+++ Network/mlp.c
@@ -16,6 +16,7 @@ typedef struct matrixstruct {
 
 typedef struct weightstruct {
   int layers; // Amount of layers in mlp
+  int *size;
   Matrix *W;  // Weights matrices
   double **b; // Bias vectors
 } * Weight;
@@ -57,28 +58,52 @@ double randRange(double min, double max) {
   return min + rand() / div;
 }
 
-MLP makeMLP(int layers, int *size) {
-  MLP model;
-  Weight weight;
+Weight makeWeight(int layers, int *size) {
+  Weight weight = safeMalloc(sizeof(struct weightstruct));
 
   weight->layers = layers;
-
+  weight->size = size;
   weight->W = safeMalloc((layers - 1) * sizeof(Matrix));
-  weight->b = safeMalloc((layers - 1) * sizeof(double));
-  model->x = safeMalloc(layers * sizeof(double));
+  weight->b = safeMalloc((layers - 1) * sizeof(double *));
 
   // Weight layer k is at k - 1
   for (int k = 0; k < layers - 1; k++) {
     weight->W[k] = makeMatrix(size[k + 1], size[k]);
-    weight->b[k] = safeMalloc(size[k + 1]);
-    model->x[k] = safeMalloc(size[k] * sizeof(double));
+    weight->b[k] = safeMalloc(size[k + 1] * sizeof(double));
   }
-  // Output layer activation units.
-  model->x[layers - 1] = safeMalloc(size[layers - 1] * sizeof(double));
-  model->weight = weight;
+  return weight;
 }
 
-double sigmoid(double x) { return 1 / (1 + exp(-x)); }
+void freeWeight(Weight weight) {
+  for (int k = 0; k < weight->layers; k++) {
+    freeMatrix(weight->W[k]);
+    free(weight->b[k]);
+  }
+  free(weight->W);
+  free(weight->b);
+  free(weight);
+}
+
+MLP makeMLP(int layers, int *size) {
+  MLP model = safeMalloc(sizeof(struct mlpstruct));
+  model->weight = makeWeight(layers, size);
+  model->x = safeMalloc(layers * sizeof(double *));
+
+  for (int k = 0; k < layers; k++) {
+    model->x[k] = safeMalloc(size[k] * sizeof(double));
+  }
+  return model;
+}
+
+void freeMLP(MLP model) {
+  int layers = model->weight->layers;
+  for (int k = 0; k < layers; k++) {
+    free(model->x[k]);
+  }
+  free(model->x);
+  freeWeight(model->weight);
+  free(model);
+}
 
 // Requires seeded rand.
 void initWeight(Weight weight) {
@@ -104,8 +129,12 @@ void initWeight(Weight weight) {
   }
 }
 
+double sigmoid(double x) { return 1 / (1 + exp(-x)); }
+
+double dMSE(double y, double yhat) { return 2 * (yhat - y); }
+
 // Input and output linear apply no sigmoid
-double *forwardMLP(MLP model, int *u) {
+double *forwardMLP(MLP model, double *u) {
   int layers, rows, columns, i, j, k;
   double *b, **x;
   Matrix W;
@@ -117,7 +146,7 @@ double *forwardMLP(MLP model, int *u) {
 
   // Set input layer with no function applied.
   columns = weight->W[0]->columns;
-  for (int i = 0; i < columns; i++) {
+  for (i = 0; i < columns; i++) {
     x[0][i] = u[i];
   }
 
@@ -152,6 +181,65 @@ double *forwardMLP(MLP model, int *u) {
   }
 
   return x[layers - 1];
+}
+
+Weight backPropMLP(MLP model, double *yhat) {
+  int layers, rows, columns, i, j, k;
+  double *b, **x, **delta;
+  Matrix W;
+  Weight weight, gradient;
+
+  weight = model->weight;
+  layers = weight->layers;
+  x = model->x;
+
+  delta = safeMalloc(layers * sizeof(double *));
+
+  k = layers - 2;
+  W = weight->W[k];
+  rows = W->rows;
+  delta[k] = safeMalloc(rows * sizeof(double));
+  for (i = 0; i < rows; i++) {
+    delta[k][i] = dMSE(x[k + 1][i], yhat[i]);
+  }
+
+  for (k = layers - 2; k > 0; k--) {
+    W = weight->W[k];
+    b = weight->b[k];
+    rows = W->rows;
+    columns = W->columns;
+
+    delta[k - 1] = safeMalloc(columns * sizeof(double));
+
+    for (i = 0; i < columns; i++) {
+      delta[k - 1][i] = 0;
+      for (j = 0; j < rows; j++) {
+        delta[k - 1][i] += delta[k][j] * W->matrix[j][i];
+      }
+      delta[k - 1][i] *= x[k][i] * (1 - x[k][i]);
+    }
+  }
+
+  // Calculating final gradient
+  gradient = makeWeight(layers, weight->size);
+  for (k = 0; k < layers - 1; k++) {
+    W = gradient->W[k];
+    b = gradient->b[k];
+    rows = W->rows;
+    columns = W->columns;
+    for (i = 0; i < rows; i++) {
+      for (j = 0; j < columns; j++) {
+        W->matrix[i][j] = delta[k][j] * x[k][i];
+      }
+      b[i] = delta[k][i];
+    }
+  }
+
+  for (k = 0; k < layers - 1; k++) {
+    free(delta[k]);
+  }
+  free(delta);
+  return gradient;
 }
 
 Matrix *readDataFile(char *fileName, int *nSeries, int *nVariate,
@@ -218,6 +306,7 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
+  fileName = argv[1];
   windowSize = intParse(argv[2]);
   nHiddenUnits = intParse(argv[3]);
 
@@ -232,10 +321,22 @@ int main(int argc, char *argv[]) {
   model = makeMLP(LAYERS, size);
   initWeight(model->weight);
 
+  double *y;
+  Weight gradient;
+  
+  y = forwardMLP(model, sample[0]->matrix[0]);
+  gradient = backPropMLP(model, y);
+
+  for (int i = 0; i < 2; i++) {
+    printf("%lf ", y[i]);
+  }
+  printf("\n");
+
   for (int i = 0; i < nSeries; i++) {
     freeMatrix(sample[i]);
   }
   free(sample);
+  free(model);
   // if (windowSize > A->rows - 1) {
   //  printf("Length of the window is too large.");
   //  exit(EXIT_FAILURE);
